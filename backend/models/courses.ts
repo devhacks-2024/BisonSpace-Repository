2a90cb7512440c3b7020157aeba23eda77901c20
Toniodujinrin <todujinrin@gmail.com>
2024-02-23 19:35:05 -0600

    created mongoose  models

---
+++ backend/models/courses.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  created: { default: Date.now, type: Number },
+  messages: [{ type: mongoose.Schema.Types.ObjectId, ref: "Message" }],
+  studyRooms: [{ type: mongoose.Schema.Types.ObjectId, ref: "StudyRoom" }],
+  users: [{ type: String, ref: "User" }],
+  history: [{ type: mongoose.Schema.Types.ObjectId, ref: "Assignment" }],
+  admin: String,
+});
+
+const Course = mongoose.model("Course", courseSchema);
+
+export default Course;
