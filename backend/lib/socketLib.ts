3bb4a90a24fba54cff57c467994144b149a9e24a
Toniodujinrin <todujinrin@gmail.com>
2024-02-24 11:11:04 -0600

    fixed ./lib issue

---
+++ backend/lib/socketLib.ts
@@ -0,0 +1,22 @@
+import Course from "../models/courses";
+import User from "../models/users";
+
+const socketLib = {
+  async getPreviousMessages(courseId: string, userId: string) {
+    const user = User.findById(userId);
+    const course = await Course.findById(courseId).populate({
+      path: "messages",
+      populate: {
+        path: "sender",
+        select: "_id firstName lastName defaultProfileColor",
+      },
+    });
+    if (!course) return [];
+    const messages = course.messages;
+    return messages;
+  },
+
+  async sendMessage(location:string, locationId:string, userId:string, body:string)
+};
+
+export default socketLib;
