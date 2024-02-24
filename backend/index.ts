4afb794fbb6f2f0c6fb3c79044058c1f3dc69e57
Toniodujinrin <todujinrin@gmail.com>
2024-02-23 20:49:14 -0600

    added authentication and registration routes

--- backend/index.ts
+++ backend/index.ts
@@ -1,1 +1,28 @@
-console.log("Hello world");
+import http from "http";
+import express from "express";
+import dotenv from "dotenv";
+import connectToDatabase from "./lib/mongoconnect";
+import rest from "./server/rest";
+import { Server } from "socket.io";
+import socketApi from "./server/socket";
+dotenv.config();
+
+try {
+  connectToDatabase();
+  const app = express();
+  let io;
+
+  rest(app);
+  const httpServer = http.createServer(app);
+  io = new Server(httpServer);
+  socketApi(io);
+
+  httpServer.listen(process.env.HTTP_PORT, () => {
+    console.log(
+      "\x1b[32m%s\x1b[0m",
+      `[+] HTTP server running on port ${process.env.HTTP_PORT}`
+    );
+  });
+} catch (error) {
+  console.log("[x] failed to start server");
+}
--- backend/models/courses.ts
+++ backend/models/courses.ts
@@ -1,7 +1,11 @@
 import mongoose from "mongoose";
 
 const courseSchema = new mongoose.Schema({
-  name: String,
+  name: {
+    officialName: String,
+    shortName: String,
+    description: String,
+  },
   created: { default: Date.now, type: Number },
   messages: [{ type: mongoose.Schema.Types.ObjectId, ref: "Message" }],
   studyRooms: [{ type: mongoose.Schema.Types.ObjectId, ref: "StudyRoom" }],
